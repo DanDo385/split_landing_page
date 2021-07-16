9f795bc86294a7f2a06829f936a04b81b8f705cf
Dando513 <49767042+Dando513@users.noreply.github.com>
2021-07-16 15:22:23 -0400

    final touches again

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
-const conainer = document.querySelector('.container')
+const container = document.querySelector('.container')
 
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
-left.addEventListener('mouseenter', () => container.classList.remove('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
-right.addEventListener('mouseenter', () => container.classList.remove('hover-right'))
-
-
-
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
 
 
 
